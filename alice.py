2df2b2210c1b0a9b4d1fd2b519a0ba00283472ec
Haden Wasserbaech <spartanhaden@gmail.com>
2018-03-23 19:54:59 -0400

    Added timeout and nonce
    Added timeout to improve testing, and added sending of bob's nonce

--- alice.py
+++ alice.py
@@ -7,11 +7,15 @@ import socket
 port = 8671
 ip = '127.0.0.1'
 
-alice_key = '23FCE5AE61E7BFCB29AC85725E7EC77DB9DBA460EACA7458070B719CE0B1DC31'
+alices_key = '23FCE5AE61E7BFCB29AC85725E7EC77DB9DBA460EACA7458070B719CE0B1DC31'
+bobs_nonce = -1
 
 
 if __name__ == '__main__':
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    '''sock.sendto(bytes('lookup ' + data, "utf-8"), (ip, port))
-    received = str(sock.recv(1024), "utf-8")
-    print('DNSLookup: ' + data + ' is ' + received)'''
+    sock.settimeout(10)
+    print('Sending \'Let\'s Talk\' to Bob')
+    sock.sendto(bytes('Let\'s talk', "utf-8"), (ip, port))
+    print('Waiting for nonce from Bob')
+    bobs_nonce = int(sock.recv(1024))
+    print('Nonce received from bob is ' + str(bobs_nonce))
--- bob.py
+++ bob.py
@@ -9,11 +9,13 @@ server_address = '127.0.0.1'
 server_port = 8671
 server = (server_address, server_port)
 
-bob_key = 'A41503A5D9E66B34FAC9F2FC9FD14CA24D728B17DE0FCC2C3676DED6A191A1F1'
+bobs_key = 'A41503A5D9E66B34FAC9F2FC9FD14CA24D728B17DE0FCC2C3676DED6A191A1F1'
+bobs_nonce = -1
 
 
 if __name__ == '__main__':
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(10)
     sock.bind(server)
     print('Listening on ' + server_address + ':' + str(server_port))
 
@@ -22,8 +24,9 @@ if __name__ == '__main__':
         payload, client_address = sock.recvfrom(1024)
 
         if payload.decode('utf-8') == 'Let\'s talk':
-            print('Let\'s talk received, sending nonce')
-            nonce = random.randint(1, 101)
-            sock.sendto(bytes(nonce, "utf-8"), client_address)
+            bobs_nonce = random.randint(1, 101)
+            print('Let\'s talk received, sending ' + str(bobs_nonce) + ' as the nonce')
+            sock.sendto(bytes(str(bobs_nonce), "utf-8"), client_address)
+            exit()
         else:
             print(payload)

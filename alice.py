aeef2b05ec4e0e961579d6122c95aacac35a7cce
Haden Wasserbaech <spartanhaden@gmail.com>
2018-03-25 00:30:10 -0400

    Removed decrypting of bobs key

--- alice.py
+++ alice.py
@@ -2,16 +2,16 @@
 
 import sys
 import socket
+import binascii
 from Crypto.Cipher import AES
+from Crypto.Random.random import getrandbits
 
 
 port = 8671
 kdc_port = 8888
 ip = '127.0.0.1'
 
-alices_key = '23FCE5AE61E7BFCB29AC85725E7EC77DB9DBA460EACA7458070B719CE0B1DC31'
-bobs_key = b'A41503A5D9E66B34FAC9F2FC9FD14CA24D728B17DE0FCC2C3676DED6A191A1F1'
-bobs_nonce = -1
+alices_key = b'23FCE5AE61E7BFCB29AC85725E7EC77DB9DBA460EACA7458070B719CE0B1DC31'
 
 
 if __name__ == '__main__':
@@ -20,17 +20,9 @@ if __name__ == '__main__':
     print('Sending "Let\'s Talk" to Bob')
     sock.sendto('Let\'s talk'.encode('utf-8'), (ip, port))
     print('Waiting for nonce from Bob')
-    payload = sock.recv(1024)
-    split_payload = payload.split(b'_')
-    ciphertext = split_payload[0]
-    tag = split_payload[1]
-    bob_cipher = AES.new(bobs_key, AES.MODE_SIV)
-    bobs_nonce = bob_cipher.decrypt_and_verify(ciphertext, tag)
-    print('Nonce received from bob is ' + str(bobs_nonce))
+    bobs_encrypted_nonce = sock.recv(1024)
+    print('Nonce received from Bob, messaging the KDC')
 
     alices_nonce = getrandbits(32)
-    kdc_message = str(bobs_nonce).encode('utf-8') + b'_'
-    kdc_message += b'Alice' + b'_'
-    kdc_message += b'Bob' + b'_'
-    kdc_message += ciphertext + b'_' + tag
+    kdc_message = str(alices_nonce).encode() + b' Alice Bob ' + bobs_encrypted_nonce
     sock.sendto(kdc_message, (ip, kdc_port))

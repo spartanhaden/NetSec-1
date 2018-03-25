747b1abf9dc12be39364a115630c7e0d1eca5382
Haden Wasserbaech <spartanhaden@gmail.com>
2018-03-24 22:13:25 -0400

    Finished Bob's first response

--- bob.py
+++ bob.py
@@ -2,14 +2,16 @@
 
 import sys
 import socket
-import random
+from Crypto.Cipher import AES
+from Crypto.Random.random import getrandbits
+import base64
 
 
 server_address = '127.0.0.1'
 server_port = 8671
 server = (server_address, server_port)
 
-bobs_key = 'A41503A5D9E66B34FAC9F2FC9FD14CA24D728B17DE0FCC2C3676DED6A191A1F1'
+bobs_key = b'A41503A5D9E66B34FAC9F2FC9FD14CA24D728B17DE0FCC2C3676DED6A191A1F1'
 bobs_nonce = -1
 
 
@@ -20,13 +22,17 @@ if __name__ == '__main__':
     print('Listening on ' + server_address + ':' + str(server_port))
 
     while True:
+        #Waits for message from Alice
+        bob_cipher = AES.new(bobs_key, AES.MODE_SIV)
         print('bob: waiting for request...')
         payload, client_address = sock.recvfrom(1024)
 
+        # Receives first messge from Alice and responds with an encrypted nonce
         if payload.decode('utf-8') == 'Let\'s talk':
-            bobs_nonce = random.randint(1, 101)
+            bobs_nonce = getrandbits(32)
             print('Let\'s talk received, sending ' + str(bobs_nonce) + ' as the nonce')
-            sock.sendto(bytes(str(bobs_nonce), "utf-8"), client_address)
+            ciphertext, tag = bob_cipher.encrypt_and_digest(str(bobs_nonce).encode('utf-8'))
+            sock.sendto(ciphertext + b'_' + tag, client_address)
             exit()
         else:
             print(payload)

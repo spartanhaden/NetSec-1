109a6631e52dbd0855f6e331b18c43889f90ca09
Haden Wasserbaech <spartanhaden@gmail.com>
2018-03-25 04:25:20 -0400

    Cleaned up encryption

--- bob.py
+++ bob.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 
-import sys
 import socket
 import binascii
 from Crypto.Cipher import AES
 from Crypto.Random.random import getrandbits
-
 
 server_address = '127.0.0.1'
 server_port = 8671
@@ -13,26 +11,49 @@ server_port = 8671
 bobs_key = b'A41503A5D9E66B34FAC9F2FC9FD14CA24D728B17DE0FCC2C3676DED6A191A1F1'
 
 
+def encrypt(key, data):
+    # Setup the cipher to encrypt the data
+    cipher = AES.new(binascii.unhexlify(key), AES.MODE_SIV)
+
+    # Encrypt the data
+    ciphertext, digest = cipher.encrypt_and_digest(data)
+
+    # Convert to ascii to comply with assignment description
+    ascii_ciphertext = binascii.hexlify(ciphertext)
+    ascii_digest = binascii.hexlify(digest)
+
+    # Concatenate the ciphertext and digest
+    return ascii_ciphertext + b'-' + ascii_digest
+
+
+def decrypt(key, data):
+    # Setup the cipher to decrypt the data
+    cipher = AES.new(binascii.unhexlify(key), AES.MODE_SIV)
+
+    # Split the ciphertext from the digest
+    ascii_ciphertext, ascii_digest = data.split(b'-')
+
+    # Decrypt the message
+    return cipher.decrypt_and_verify(binascii.unhexlify(ascii_ciphertext), binascii.unhexlify(ascii_digest))
+
+
 if __name__ == '__main__':
+    # Setup socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.settimeout(10)
     sock.bind((server_address, server_port))
     print('Listening on ' + server_address + ':' + str(server_port))
 
     while True:
-        #Waits for message from Alice
-        bob_cipher = AES.new(bobs_key, AES.MODE_SIV)
-        print('bob: waiting for request...')
-        payload, client_address = sock.recvfrom(1024)
+        # Waits for message from Alice
+        print('Bob: waiting for request...')
+        payload, client_address = sock.recvfrom(4096)
 
-        # Receives first messge from Alice and responds with an encrypted nonce
-        if payload.decode('utf-8') == 'Let\'s talk':
+        # Receives first message from Alice and responds with an encrypted nonce
+        if payload.decode() == 'Let\'s talk':
             bobs_nonce = getrandbits(32)
             print('Let\'s talk received, sending ' + str(bobs_nonce) + ' as the nonce')
-            ciphertext, tag = bob_cipher.encrypt_and_digest(str(bobs_nonce).encode('utf-8'))
-            ascii_ciphertext = binascii.hexlify(ciphertext)
-            ascii_tag = binascii.hexlify(tag)
-            message = ascii_ciphertext + b'-' + ascii_tag
+            message = encrypt(bobs_key, str(bobs_nonce).encode())
             sock.sendto(message, client_address)
             #exit()
         else:

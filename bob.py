e6df9ca06bcd52bac75f107b1a25a92a89e3f64b
Haden Wasserbaech <spartanhaden@gmail.com>
2018-03-25 00:28:53 -0400

    Sending nonce as ASCII

--- bob.py
+++ bob.py
@@ -2,23 +2,21 @@
 
 import sys
 import socket
+import binascii
 from Crypto.Cipher import AES
 from Crypto.Random.random import getrandbits
-import base64
 
 
 server_address = '127.0.0.1'
 server_port = 8671
-server = (server_address, server_port)
 
 bobs_key = b'A41503A5D9E66B34FAC9F2FC9FD14CA24D728B17DE0FCC2C3676DED6A191A1F1'
-bobs_nonce = -1
 
 
 if __name__ == '__main__':
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.settimeout(10)
-    sock.bind(server)
+    sock.bind((server_address, server_port))
     print('Listening on ' + server_address + ':' + str(server_port))
 
     while True:
@@ -32,7 +30,10 @@ if __name__ == '__main__':
             bobs_nonce = getrandbits(32)
             print('Let\'s talk received, sending ' + str(bobs_nonce) + ' as the nonce')
             ciphertext, tag = bob_cipher.encrypt_and_digest(str(bobs_nonce).encode('utf-8'))
-            sock.sendto(ciphertext + b'_' + tag, client_address)
-            exit()
+            ascii_ciphertext = binascii.hexlify(ciphertext)
+            ascii_tag = binascii.hexlify(tag)
+            message = ascii_ciphertext + b'-' + ascii_tag
+            sock.sendto(message, client_address)
+            #exit()
         else:
             print(payload)

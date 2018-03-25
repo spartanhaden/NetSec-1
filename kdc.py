7239af67d6819d0e9d070008ecc328f7ad8062f3
Haden Wasserbaech <spartanhaden@gmail.com>
2018-03-25 00:30:29 -0400

    Finished KDC reading of information

--- kdc.py
+++ kdc.py
@@ -2,27 +2,42 @@
 
 import sys
 import socket
+from Crypto.Cipher import AES
+from Crypto.Random.random import getrandbits
 
 
-port = 8888
+server_address = '127.0.0.1'
+server_port = 8888
 
-alice_key = '23FCE5AE61E7BFCB29AC85725E7EC77DB9DBA460EACA7458070B719CE0B1DC31'
-bob_key = 'A41503A5D9E66B34FAC9F2FC9FD14CA24D728B17DE0FCC2C3676DED6A191A1F1'
-session_key = 'V34UT5YH4UYDT5FG3UX5Y3J4CXYX3RYDTUC5LT3UC45TUVGJ3YU4V5HBJB4J3V5U'
+alices_key = b'23FCE5AE61E7BFCB29AC85725E7EC77DB9DBA460EACA7458070B719CE0B1DC31'
+bobs_key = b'A41503A5D9E66B34FAC9F2FC9FD14CA24D728B17DE0FCC2C3676DED6A191A1F1'
+
+
+def create_ticket(bobs_encrypted_nonce):
+    pass
+
 
 if __name__ == '__main__':
-    '''sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind(server)
-    print("Listening on " + server_address + ":" + str(server_port))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(10)
+    sock.bind((server_address, server_port))
+    print('Listening on ' + server_address + ':' + str(server_port))
 
     while True:
-        print('DNSResolver: waiting for request...')
+        # Waits for a message from Alice
+        print('KDC: waiting for request...')
         payload, client_address = sock.recvfrom(1024)
-        request = payload.decode("utf-8").split(' ')
 
-        if request[0] == 'lookup':
-            print('DNSResolver: Request received - ' + request[1])
+        split_payload = payload.split(b' ')
+        if len(split_payload) != 4:
+            print("nope")
+            continue
+        alices_nonce = split_payload[0]
+        bobs_encrypted_nonce = split_payload[3]
 
-            if request[1] in dns_table:
-                print('DNSResolver: Responding with ' + dns_table[request[1]])
-                sock.sendto(bytes(dns_table[request[1]], "utf-8"), client_address)'''
+        if split_payload[1] == b'Alice' and split_payload[2] == b'Bob':
+            print("Message from Alice received, sending response")
+
+            #bob_cipher = AES.new(bobs_key, AES.MODE_SIV)
+            #bobs_nonce = bob_cipher.decrypt_and_verify(ciphertext, tag)
+            #print('Nonce received from bob is ' + str(bobs_nonce))

f790420a07978b9becab190b4aab9785f3ab79a4
Haden Wasserbaech <spartanhaden@gmail.com>
2018-03-25 04:24:53 -0400

    Alice now messages with the KDC properly

--- alice.py
+++ alice.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import sys
 import socket
 import binascii
 from Crypto.Cipher import AES
@@ -14,15 +13,53 @@ ip = '127.0.0.1'
 alices_key = b'23FCE5AE61E7BFCB29AC85725E7EC77DB9DBA460EACA7458070B719CE0B1DC31'
 
 
+def encrypt(key, data):
+    # Setup the cipher to encrypt the data
+    cipher = AES.new(binascii.unhexlify(key), AES.MODE_SIV)
+
+    # Encrypt the data
+    ciphertext, digest = cipher.encrypt_and_digest(data)
+
+    # Convert to ascii to comply with assignment description
+    ascii_ciphertext = binascii.hexlify(ciphertext)
+    ascii_digest = binascii.hexlify(digest)
+
+    # Concatenate the ciphertext and digest
+    return ascii_ciphertext + b'-' + ascii_digest
+
+
+def decrypt(key, data):
+    # Setup the cipher to decrypt the data
+    cipher = AES.new(binascii.unhexlify(key), AES.MODE_SIV)
+
+    # Split the ciphertext from the digest
+    ascii_ciphertext, ascii_digest = data.split(b'-')
+
+    # Decrypt the message
+    return cipher.decrypt_and_verify(binascii.unhexlify(ascii_ciphertext), binascii.unhexlify(ascii_digest))
+
+
 if __name__ == '__main__':
+    # Setup socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.settimeout(10)
+
+    # Send initial message to Bob
     print('Sending "Let\'s Talk" to Bob')
-    sock.sendto('Let\'s talk'.encode('utf-8'), (ip, port))
+    sock.sendto('Let\'s talk'.encode(), (ip, port))
+
+    # Receive message back from Bob
     print('Waiting for nonce from Bob')
-    bobs_encrypted_nonce = sock.recv(1024)
+    bobs_encrypted_nonce = sock.recv(4096)
     print('Nonce received from Bob, messaging the KDC')
 
     alices_nonce = getrandbits(32)
     kdc_message = str(alices_nonce).encode() + b' Alice Bob ' + bobs_encrypted_nonce
     sock.sendto(kdc_message, (ip, kdc_port))
+
+    kdc_response = sock.recv(4096)
+    decrypted_kdc = decrypt(alices_key, kdc_response)
+    print(decrypted_kdc)
+
+    # bobs_nonce = bob_cipher.decrypt_and_verify(ciphertext, tag)
+    # print('Nonce received from bob is ' + str(bobs_nonce))
